a7ff881e213d03d163968791886c443b136bf4c1
Boka44 <Guitar101!>
2018-03-16 14:03:30 -0700

    edited text/html response

--- routes/barjoker.js
+++ routes/barjoker.js
@@ -24,15 +24,40 @@ app.post('/', function (req, res) {
     req.headers['Access-Control-Allow-Origin'] = '*'
 	let email = req.body.email;
 	let name = req.body.name;
-	let text = `Hello ${name},\n\n Welcome to Barjoker!`;
+	let text = `Hello ${name},\n\n Welcome to Barjoker! 
+	The game where you put your friends in uncomofortable situations, for fun!
+	\n\nMy name is Boka, the creator of Barjoker, and I would like to welcome you personally. 
+	\n\nThe current version is a prototype, version 0.1.0, so expect more features soon. If you have any questions,
+	reply to this email and I will get back to you personally.
+	\n\n\nCheers!
+	\n\nBoka`;
+	let html = `<h2>Hello ${name},<h2>
+					<br>
+					<p>Welcome to <div style="color: 'red'">Barjoker!</div> The game where you put your friends in uncomofortable situations, for fun!</p>>
+					<br>
+					<br>
+					<p>My name is Boka, the creator of Barjoker, and I would like to welcome you personally.</p>
+					<br>
+					<br>
+					<p>The current version is a prototype, version 0.1.0, so expect more features soon. If you have any questions,
+	reply to this email and I will get back to you personally.</p>
+					<br>
+					<br>
+					<br>
+					<h4>Cheers</h4>
+					<br>
+					<br>
+					<p><strong>Boka</strong></p>`
+
 	
 
   // const from = name && email ? `${name} <${email}>` : `${name || email}`
   let mailOptions = {
     from: 'nhrboka@aol.com',
-    to: email,
+    to: `${email}`,
     subject: `Welcome to Barjoker!`,
-    text
+    text,
+    html
   };
 
  transporter.sendMail(mailOptions, function(error, info){
